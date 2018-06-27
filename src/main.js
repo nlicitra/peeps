c78f82cac02f3baccaee5c0e737cf491bc4ac225
nlicitra <nickerous@gmail.com>
2018-06-27 14:34:18 -0400

    only render when visible and touch state

--- src/main.js
+++ src/main.js
@@ -10,19 +10,48 @@ const stream = new AudioStream(PeepsMP3);
 
 setTimeout(() => {
     const logos = document.querySelector('.logos-container');
+    logos.addEventListener('touchstart', (event) => {
+        event.preventDefault();
+        stream.toggle();
+    });
     logos.addEventListener('click', () => {
+        event.preventDefault();
         stream.toggle();
     });
 }, 0);
+
+var hidden, visibilityChange;
+if (typeof document.hidden !== 'undefined') {
+    // Opera 12.10 and Firefox 18 and later support
+    hidden = 'hidden';
+    visibilityChange = 'visibilitychange';
+} else if (typeof document.msHidden !== 'undefined') {
+    hidden = 'msHidden';
+    visibilityChange = 'msvisibilitychange';
+} else if (typeof document.webkitHidden !== 'undefined') {
+    hidden = 'webkitHidden';
+    visibilityChange = 'webkitvisibilitychange';
+}
+
+let visible = true;
+document.addEventListener(
+    visibilityChange,
+    () => {
+        visible = !document[hidden];
+    },
+    false,
+);
 
 stream.makeBand('low', {
     highPass: 200,
     lowPass: 150,
     callback: () => {
-        renderer.burst(BurgerPNG);
-        renderer.burst(BurgerPNG);
-        renderer.burst(BurgerPNG);
-        renderer.burst(BurgerPNG);
+        if (visible) {
+            renderer.burst(BurgerPNG);
+            renderer.burst(BurgerPNG);
+            renderer.burst(BurgerPNG);
+            renderer.burst(BurgerPNG);
+        }
     },
 });
 stream.makeBand('mid', {
@@ -30,10 +59,12 @@ stream.makeBand('mid', {
     lowPass: 2000,
     threshold: 200,
     callback: () => {
-        renderer.burst(PeepPNG);
-        renderer.burst(PeepPNG);
-        renderer.burst(PeepPNG);
-        renderer.burst(PeepPNG);
+        if (visible) {
+            renderer.burst(PeepPNG);
+            renderer.burst(PeepPNG);
+            renderer.burst(PeepPNG);
+            renderer.burst(PeepPNG);
+        }
     },
 });
 stream.makeBand('high', {
