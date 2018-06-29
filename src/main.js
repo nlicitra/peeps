938e4eb3e6d0e4eeb580f6685e6530963b30ee76
nlicitra <nickerous@gmail.com>
2018-06-28 21:27:49 -0400

    mobile support

--- src/main.js
+++ src/main.js
@@ -3,21 +3,67 @@ import Renderer from './Renderer';
 import PeepsMP3 from '../assets/peeps.mp3';
 import PeepPNG from '../assets/peep.png';
 import BurgerPNG from '../assets/burgerman.png';
+import {mobileAndTabletCheck} from './utils';
+
+const isMobile = mobileAndTabletCheck();
 
 const renderer = new Renderer();
 
-const stream = new AudioStream(PeepsMP3);
+const initAudio = () => {
+    const stream = new AudioStream(PeepsMP3);
+    stream.makeBand('low', {
+        highPass: 200,
+        lowPass: 150,
+        callback: () => {
+            if (visible) {
+                renderer.burst(BurgerPNG);
+                renderer.burst(BurgerPNG);
+                renderer.burst(BurgerPNG);
+                renderer.burst(BurgerPNG);
+            }
+        },
+    });
+    stream.makeBand('mid', {
+        highPass: 1000,
+        lowPass: 2000,
+        threshold: 200,
+        callback: () => {
+            if (visible) {
+                renderer.burst(PeepPNG);
+                renderer.burst(PeepPNG);
+                renderer.burst(PeepPNG);
+                renderer.burst(PeepPNG);
+            }
+        },
+    });
+    stream.makeBand('high', {
+        highPass: 5000,
+        threshold: 200,
+        callback: () => {},
+    });
+
+    return stream;
+};
+
+let stream = null;
 
 setTimeout(() => {
     const logos = document.querySelector('.logos-container');
-    logos.addEventListener('touchstart', (event) => {
-        event.preventDefault();
-        stream.toggle();
-    });
-    logos.addEventListener('click', (event) => {
-        event.preventDefault();
-        stream.toggle();
-    });
+    if (isMobile) {
+        logos.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            if (!stream) {
+                stream = initAudio();
+            }
+            stream.toggle();
+        });
+    } else {
+        stream = initAudio();
+        logos.addEventListener('click', (event) => {
+            event.preventDefault();
+            stream.toggle();
+        });
+    }
 }, 0);
 
 var hidden, visibilityChange;
@@ -41,34 +87,3 @@ document.addEventListener(
     },
     false,
 );
-
-stream.makeBand('low', {
-    highPass: 200,
-    lowPass: 150,
-    callback: () => {
-        if (visible) {
-            renderer.burst(BurgerPNG);
-            renderer.burst(BurgerPNG);
-            renderer.burst(BurgerPNG);
-            renderer.burst(BurgerPNG);
-        }
-    },
-});
-stream.makeBand('mid', {
-    highPass: 1000,
-    lowPass: 2000,
-    threshold: 200,
-    callback: () => {
-        if (visible) {
-            renderer.burst(PeepPNG);
-            renderer.burst(PeepPNG);
-            renderer.burst(PeepPNG);
-            renderer.burst(PeepPNG);
-        }
-    },
-});
-stream.makeBand('high', {
-    highPass: 5000,
-    threshold: 200,
-    callback: () => {},
-});
